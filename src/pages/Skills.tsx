64fad6cca86689a93c2b6bcd16885982635213dd
Edra Prasetio <edraadrianp@gmail.com>
2024-05-27 21:49:13 -0700

    Add figma icon

--- src/pages/Skills.tsx
+++ src/pages/Skills.tsx
@@ -8,6 +8,7 @@ import c from '../assets/icons/skills/c-1.svg'
 import cpp from '../assets/icons/skills/c.svg'
 import css from '../assets/icons/skills/css-3.svg'
 import django from '../assets/icons/skills/django (1).svg'
+import figma from '../assets/icons/skills/figma-icon.svg'
 import git from '../assets/icons/skills/git-bash.svg'
 import go from '../assets/icons/skills/go-6.svg'
 import html from '../assets/icons/skills/html-1.svg'
@@ -31,20 +32,21 @@ export function Skills() {
         { id: 4, name: 'C++', category: 'Programming Languages', yearsOfExperience: 1, imageUrl: cpp },
         { id: 5, name: 'CSS', category: 'Development Tools', yearsOfExperience: 2, imageUrl: css },
         { id: 6, name: 'Django', category: 'Development Tools', yearsOfExperience: 3, imageUrl: django },
-        { id: 7, name: 'Git', category: 'Development Tools', yearsOfExperience: 5, imageUrl: git },
-        { id: 8, name: 'Go', category: 'Programming Languages', yearsOfExperience: 3, imageUrl: go },
-        { id: 9, name: 'HTML', category: 'Development Tools', yearsOfExperience: 2, imageUrl: html },
-        { id: 10, name: 'Java', category: 'Programming Languages', yearsOfExperience: 5, imageUrl: java },
-        { id: 11, name: 'JavaScript', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: javaScript },
-        { id: 12, name: 'MongoDB', category: 'Databases', yearsOfExperience: 1, imageUrl: mongodb },
-        { id: 13, name: 'MySQL', category: 'Databases', yearsOfExperience: 3, imageUrl: mysql },
-        { id: 14, name: 'Node.js', category: 'Development Tools', yearsOfExperience: 2, imageUrl: nodeJS },
-        { id: 15, name: 'PostgreSQL', category: 'Databases', yearsOfExperience: 3, imageUrl: postgress },
-        { id: 16, name: 'Python', category: 'Programming Languages', yearsOfExperience: 5, imageUrl: python },
-        { id: 17, name: 'R', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: r },
-        { id: 18, name: 'React', category: 'Development Tools', yearsOfExperience: 2, imageUrl: react },
-        { id: 19, name: 'TypeScript', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: typeScript },
-        { id: 20, name: 'WordPress', category: 'Development Tools', yearsOfExperience: 0.5, imageUrl: wordPress },
+        { id: 7, name: 'Figma', category: 'Development Tools', yearsOfExperience: 3, imageUrl: figma },
+        { id: 8, name: 'Git', category: 'Development Tools', yearsOfExperience: 5, imageUrl: git },
+        { id: 9, name: 'Go', category: 'Programming Languages', yearsOfExperience: 3, imageUrl: go },
+        { id: 10, name: 'HTML', category: 'Development Tools', yearsOfExperience: 2, imageUrl: html },
+        { id: 11, name: 'Java', category: 'Programming Languages', yearsOfExperience: 5, imageUrl: java },
+        { id: 12, name: 'JavaScript', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: javaScript },
+        { id: 13, name: 'MongoDB', category: 'Databases', yearsOfExperience: 1, imageUrl: mongodb },
+        { id: 14, name: 'MySQL', category: 'Databases', yearsOfExperience: 3, imageUrl: mysql },
+        { id: 15, name: 'Node.js', category: 'Development Tools', yearsOfExperience: 2, imageUrl: nodeJS },
+        { id: 16, name: 'PostgreSQL', category: 'Databases', yearsOfExperience: 3, imageUrl: postgress },
+        { id: 17, name: 'Python', category: 'Programming Languages', yearsOfExperience: 5, imageUrl: python },
+        { id: 18, name: 'R', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: r },
+        { id: 19, name: 'React', category: 'Development Tools', yearsOfExperience: 2, imageUrl: react },
+        { id: 20, name: 'TypeScript', category: 'Programming Languages', yearsOfExperience: 2, imageUrl: typeScript },
+        { id: 21, name: 'WordPress', category: 'Development Tools', yearsOfExperience: 0.5, imageUrl: wordPress },
     ]
 
     return (
